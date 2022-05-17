a4b7c3f46031bb58841d2c33bb3329b1e1f03e3c
Ksawery <Ksawi.kowalczyk@wp.pl>
2022-05-17 12:48:46 +0200

    modify goal

--- Goal.py
+++ Goal.py
@@ -4,5 +4,5 @@ class Goal(Player.Player):
     def __init__(self, character):
         super().__init__(character)
     def Move(self):
-        self._pos_x = random(0, 3)
-        self._pos_y = random(0, 3)
+        self._pos_x = random.randint(0, 3)
+        self._pos_y = random.randint(0, 3)
