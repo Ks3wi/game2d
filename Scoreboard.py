594f2a4056577d8c7832cadba950ba2a424888c3
Ksawery <Ksawi.kowalczyk@wp.pl>
2022-05-24 12:43:25 +0200

    added scoreboard, display, fill

---
+++ Scoreboard.py
@@ -0,0 +1,17 @@
+class Scoreboard:
+    def __init__(self):
+        self.__scoreboard = {}
+        self.FillTheScoreboard()
+    def FillTheScoreboard(self):
+        file = open("score.txt", "r")
+        content = file.read()
+        file.close
+        list_content = content.split(' ')
+        i = 0
+        while i < len(list_content):
+            self.__scoreboard[list_content[i]] = int(list_content[i + 1])
+            i += 2
+    def Display(self):
+        for key in self.__scoreboard:
+            print(key + ": " + str(self.__scoreboard[key]))
+    
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import Board
 import Goal
 import User
 import game
+import Scoreboard
 
-Game1 = game.Game('X', 'player1', 'o')
-Game1.Play()
+#Game1 = game.Game('X', 'player1', 'o')
+#Game1.Play()
 
-
+S1 = Scoreboard.Scoreboard()
+S1.Display()
 
